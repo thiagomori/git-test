d26e1892a80f0fcd90b018d51a0dafd775ac5302
Thiago Mori <thi_ago_5@hotmail.com>
2015-03-04 23:57:43 -0300

    Exercicio 5(Teste)

--- teste.php
+++ teste.php
@@ -2,6 +2,11 @@
     $x=5;
     $y=5;
     
-    <?php include"operacoes.php"?>
-    echo (int)
+    <?php include "operacoes.php" ?>
+    function imprime_resultado($resultado) {
+    echo "O resultado : ".$resultado;
+    $resultado = soma($x, $y);
+    imprime_resultado($resultado);
+
+
 ?>
